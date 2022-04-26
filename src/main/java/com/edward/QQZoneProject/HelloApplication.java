0ffb1322161becaead34bd9d31777c2ef148f149
edward <xsd1115637240@gmail.com>
2022-04-26 20:25:08 +0800

    QQZoneProject 26/4/2022 first

---
+++ src/main/java/com/edward/QQZoneProject/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.edward.QQZoneProject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
